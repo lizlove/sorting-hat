0769d7510d66af14a8ff8a0885f66ac2464791c9
lizlove <elizabethlovero@gmail.com>
2016-07-27 16:25:04 -0400

    Update sorting hat landing image.

--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,8 @@ module.exports = function(app, express) {
   app.get("/", function(req, res) {
       res.render('index');
   });
+  // main API route for sorting
+  app.get("/:version/sort", function(req, res){
+
+  });
 };
